6500e9ad51fa5ee075b8468981f7d4c98e2f2d0c
Marlon Silveira Gomes <marlonsilveiragomes@gmail.com>
2024-11-04 19:00:24 -0300

    Atualização - Consumindo API - Lidando com Exceções

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,12 +1,31 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo {
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
+import com.google.gson.annotations.SerializedName;
+
+public class Titulo implements Comparable<Titulo> {
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
+
+    public Titulo(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length()> 4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano porque " +
+                    "tem mais de 04 caractreres");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+    }
 
     public String getNome() {
         return nome;
@@ -57,4 +76,18 @@ public class Titulo {
     public double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome= '" + nome + '\'' +
+                ", anoDeLancamento= " + anoDeLancamento + "," +
+                " duração= " + duracaoEmMinutos;
+    }
 }
+
--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -1,4 +1,50 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.*;
+
 public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        outroFilme.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            if (item instanceof  Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+
+        }
+        ArrayList<String> buscarArtista = new ArrayList<>();
+        buscarArtista.add("Adam Sandler");
+        buscarArtista.add("Paulo");
+        buscarArtista.add("Jacqueline");
+        System.out.println(buscarArtista );
+
+        Collections.sort(buscarArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscarArtista);
+        System.out.println("Lista de titulos ordenadas");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenado por ano");
+        System.out.println(lista);
+
+
+    }
 }
